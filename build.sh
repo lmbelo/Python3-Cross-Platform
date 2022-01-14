eae242ed4a4168ca515f933c470128ce60154fac
lmbelo <lucas.belo@live.com>
2022-01-13 16:43:22 -0800

    Building options for multiple Python versions

--- build.sh
+++ build.sh
@@ -24,6 +24,8 @@ cp -r Android $SRCDIR
 pushd $SRCDIR
 patch -Np1 -i ./Android/unversioned-libpython.patch
 autoreconf -ifv
+which python
+python -m pip install dataclasses
 ./Android/build_deps.py $COMMON_ARGS
 ./Android/configure.py $COMMON_ARGS --prefix=/usr "$@"
 make
