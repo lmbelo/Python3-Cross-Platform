3427c2c532561b57abb09d8ca03281dd9d5100b4
lmbelo <lucas.belo@live.com>
2022-04-22 13:13:30 -0700

    Targeting MacOS

--- python3-macos/docker-build.sh
+++ python3-macos/docker-build.sh
@@ -4,10 +4,16 @@ set -e
 set -x
 
 apt-get update -y
-apt-get install -y autoconf autoconf-archive automake cmake gawk gettext git gcc llvm clang lld make patch pkg-config
+apt-get install clang llvm-dev libxml2-dev uuid-dev libssl-dev bash patch make tar xz-utils bzip2 gzip sed cpio libbz2-dev
 
-export LINUX_NDK=/usr/bin
+export MACOS_SDK=/android-ndk
 
-cd /python3-linux
+if [ ! -d "$ANDROID_NDK" ] ; then
+    cd /cross-toolchain/tarballs
+    curl -vLO https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.14.sdk.tar.xz
+    cd ../../
+fi
 
-./build.sh "$@"
+cd /python3-macos
+
+#./build.sh "$@"
