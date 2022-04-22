f04bb0afd82c821a8cfb8d6b3571ea5209e6eb5e
lmbelo <lucas.belo@live.com>
2022-04-22 13:59:37 -0700

    Targeting MacOS

--- python3-macos/docker-build.sh
+++ python3-macos/docker-build.sh
@@ -6,7 +6,7 @@ set -x
 apt-get update -y
 apt-get install -y git curl cmake patch libssl-dev lzma-dev libxml2-dev llvm-dev
 
-curl -sSL https://cmake.org/files/v3.14/cmake-3.14.5-Linux-x86_64.tar.gz | sudo tar -xzC /opt
+curl -sSL https://cmake.org/files/v3.14/cmake-3.14.5-Linux-x86_64.tar.gz | tar -xzC /opt
 export PATH=/opt/cmake-3.14.5-Linux-x86_64/bin:$PATH
 
 cd python3-macos/cross-toolchain
