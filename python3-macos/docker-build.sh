b38c25aad618d27e336282caf0121daf4acd6437
lmbelo <lucas.belo@live.com>
2022-04-22 13:28:35 -0700

    Targeting MacOS

--- python3-macos/docker-build.sh
+++ python3-macos/docker-build.sh
@@ -8,13 +8,11 @@ apt-get install -y git curl clang llvm-dev libxml2-dev uuid-dev libssl-dev bash 
 
 export MACOS_SDK=/macos-sdk
 
-ls -l
-
 if [ ! -d "$ANDROID_NDK" ] ; then
     curl -vLO https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.14.sdk.tar.xz --output python3-macos/cross-toolchain/tarballs/MacOSX10.14.sdk.tar.xz
 fi
 
-ls -R
+ls python3-macos/cross-toolchain/tarballs
 
 cd /python3-macos
 
