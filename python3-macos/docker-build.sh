f3df750c7fbbb58e95371fe5efc53eb97acd42fe
lmbelo <lucas.belo@live.com>
2022-04-25 16:46:22 -0700

    Targeting macos

--- python3-macos/docker-build.sh
+++ python3-macos/docker-build.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 apt-get update -y
-apt-get install -y autoconf autoconf-archive automake curl wget patch tar xz-utils bzip2 gzip libssl-dev lzma-dev libxml2-dev
+apt-get install -y autoconf autoconf-archive automake curl wget patch tar xz-utils bzip2 gzip make libssl-dev lzma-dev libxml2-dev
 
 # We must upgrade CMake to >= 3.2.3 first
 curl -sSL https://cmake.org/files/v3.14/cmake-3.14.5-Linux-x86_64.tar.gz | tar -xzC /opt
